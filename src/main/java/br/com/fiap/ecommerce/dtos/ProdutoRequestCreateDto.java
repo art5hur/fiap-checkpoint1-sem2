ba6f820cabab194f22b922e94f29d43fcfc8e5ca
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-27 20:36:18 -0300

    Inicio CP

---
+++ src/main/java/br/com/fiap/ecommerce/dtos/ProdutoRequestCreateDto.java
@@ -0,0 +1,35 @@
+package br.com.fiap.ecommerce.dtos;
+
+import java.math.BigDecimal;
+
+import org.modelmapper.ModelMapper;
+
+import br.com.fiap.ecommerce.model.Produto;
+
+public class ProdutoRequestCreateDto {
+    private String nome;
+    private BigDecimal valor;
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    
+    public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Produto toModel() {
+        return modelMapper.map(this, Produto.class);
+    }
+    
+}
